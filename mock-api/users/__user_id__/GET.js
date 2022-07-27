702a74678242a121d71245da5c75abc8647ec922
TranSyHuy <syhuy01072002@gmail.com>
2022-07-27 19:34:42 +0700

    first-commit

--- mock-api/users/__user_id__/GET.js
+++ mock-api/users/__user_id__/GET.js
@@ -1,10 +1,10 @@
-module.exports = function(request, response) {
-  let users = require("../../users/GET.json");
+module.exports = function (request, response) {
+  let users = require("../GET.json");
 
-  let user = users.find(user => user.id == request.params.user_id);
+  let user = users.find((user) => user.id == request.params.user_id);
 
   response.json({
     id: user.id,
-    name: user.name
+    name: user.name,
   });
 };
